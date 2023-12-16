6e79faefd61d853cc71c41bdc4d308413d64847d
Felix Semente <142948784+Felix-Semente@users.noreply.github.com>
2023-12-15 23:51:33 -0300

    Add files via upload

---
+++ form_produto.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="cad_produto.php" method="post">
+        <label for="">nome: </label>
+        <input type="text" name="nome">
+        <label for="">descrição: </label>
+        <input type="text" name="description">
+        <label for="">lote: </label>
+        <input type="number" name="lote">
+        <input type="submit">
+
+    </form>
+</body>
+
+</html>
